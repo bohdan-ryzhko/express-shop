9d3f7a7b38c1790ca4fe9c269197a44fcae506b9
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-03-21 01:25:08 +0200

    add header logo

--- src/components/Header/HeaderLogo/HeaderLogo.jsx
+++ src/components/Header/HeaderLogo/HeaderLogo.jsx
@@ -1,11 +1,12 @@
-// import logo from "images/logo/logo.png";
+import headerLogo from '../../../images/logo/logo-1.png';
 import sass from './HeaderLogo.module.scss';
+
 
 export const HeaderLogo = () => {
   return (
     <div className={sass.header__logo}>
       <a className={sass.header__logoLink} href="./index.html">
-        <img width={150} height={150} src="" alt="Logo" />
+        <img width={100} height={100} src={headerLogo} alt="Logo" />
       </a>
     </div>
   );
