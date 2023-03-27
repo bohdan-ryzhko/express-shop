56d85d92c4351c5fe9155a9fa88599ed194d5019
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-03-27 14:48:28 +0300

    remove code test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,12 @@ import { Fragment } from 'react';
 // import { MainPage } from './MainPage/MainPage';
 
 import headerUrls from '../data/header_link';
-// import underwear from "../data/underwear";
+import underwear from "../data/underwear";
 // import bags from "../data/bags";
-// import { ProductList } from './ProductList/ProductList';
-import { FormOrder } from './Form/FormOrder';
-import PersonalPizza from './Clock/Clock';
+import { ProductList } from './ProductList/ProductList';
+// import { FormOrder } from './Form/FormOrder';
 
-// const { yml_catalog: { shop: { categories: { category: { underwearTitle } }, offers: { underwearOffer } } } } = underwear;
+const { yml_catalog: { shop: { categories: { category: { underwearTitle } }, offers: { underwearOffer } } } } = underwear;
 // const { yml_catalog: { shop: { categories: { category: { bagsTitlte } }, offers: { bagsOffer } } } } = bags;
 
 export const App = () => {
@@ -18,12 +17,10 @@ export const App = () => {
     <Fragment>
       <Header items={headerUrls} />
       {/* <MainPage /> */}
-      {/* <ProductList title={underwearTitle} list={underwearOffer} /> */}
+      <ProductList title={underwearTitle} list={underwearOffer} />
       {/* <ProductList title={bagsTitlte} list={bagsOffer} /> */}
-      <FormOrder />
+      {/* <FormOrder /> */}
       <Footer />
-
-      <PersonalPizza/>
     </Fragment>
   );
 };
