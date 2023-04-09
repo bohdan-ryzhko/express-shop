3f6ad4298974fa0f6cc320135565be161ecb07f0
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-04-09 22:56:15 +0300

    html + low styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
 import { Header } from './Header/Header';
 import { Footer } from './Footer/Footer';
 import { Fragment } from 'react';
+import Card from './Card/Card';
 // import { MainPage } from './MainPage/MainPage';
 
 import headerUrls from '../data/header_link';
-import underwear from "../data/underwear";
+import underwear from '../data/underwear';
 // import bags from "../data/bags";
 import { ProductList } from './ProductList/ProductList';
 
-const { yml_catalog: { shop: { categories: { category: { underwearTitle } }, offers: { underwearOffer } } } } = underwear;
+const {
+  yml_catalog: {
+    shop: {
+      categories: {
+        category: { underwearTitle },
+      },
+      offers: { underwearOffer },
+    },
+  },
+} = underwear;
 // const { yml_catalog: { shop: { categories: { category: { bagsTitlte } }, offers: { bagsOffer } } } } = bags;
 
 export const App = () => {
   return (
     <Fragment>
       <Header items={headerUrls} />
+      <Card />
       {/* <MainPage /> */}
       <ProductList title={underwearTitle} list={underwearOffer} />
       {/* <ProductList title={bagsTitlte} list={bagsOffer} /> */}
