78eff54569b898fc2b51e4a5f2d836795625a72f
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-04-19 14:53:25 +0300

    add count products on basket btn

--- src/components/BasketButton/BasketButton.jsx
+++ src/components/BasketButton/BasketButton.jsx
@@ -1,12 +1,17 @@
 import { BsBasket2Fill } from "react-icons/bs";
 
 import sass from "./BasketButton.module.scss";
+import { useSelector } from "react-redux";
 
 export const BasketButton = ({ setToggleBasket }) => {
+
+	const orderList = useSelector(state => state.orderList);
+
 	return (
 		<div className={sass.basket}>
 			<button onClick={() => setToggleBasket(prev => !prev)} className={sass.basket__btn}>
-				<BsBasket2Fill size={20} />
+				<span className={sass.countProducts}>{orderList.length}</span>
+				<BsBasket2Fill size={35} />
 			</button>
 		</div>
 	)
